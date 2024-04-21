a0de7989c90010c769445625cdcf014906ec3025
BMiller2k2 <rbrianmiller@gmail.com>
2024-04-21 14:10:20 -0400

    setting up background image and game canvas edited index to reflect the phaser framework better.

---
+++ PreloadScene.js
@@ -0,0 +1,10 @@
+class Scene1 extends Phaser.Scene {
+    constructor() {
+      super("bootGame");
+    }
+
+
+    preload(){
+        this.load.image("background", "assets/images/background.png");
+    }
+}
