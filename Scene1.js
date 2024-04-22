536426762c3b7151de6d5d87253574ddd31ec966
BMiller2k2 <rbrianmiller@gmail.com>
2024-04-22 15:50:23 -0400

    change background back

--- Scene1.js
+++ Scene1.js
@@ -4,7 +4,7 @@ class Scene1 extends Phaser.Scene {
   }
 
   preload(){
-    this.load.image("background", "assets/images/background1.png");
+    this.load.image("background", "assets/images/background.png");
 
     this.load.spritesheet("ship", "assets/spritesheets/ship.png",{
       frameWidth: 16,
