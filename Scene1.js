ac4fb036f54ae307feb21ac7269e1536f1debb33
bohmanwl <112824679+bohmanwl@users.noreply.github.com>
2024-04-24 16:55:51 -0400

    Two variation of the game exitst, desert and ocean. The version of played game is decided by the temperature by the user

--- Scene1.js
+++ Scene1.js
@@ -4,16 +4,15 @@ class Scene1 extends Phaser.Scene {
     super("bootGame");
   }
     preload(){
-
-
-     // Sets the game background based on the users Latitude provided by their browser
-     function getUserLatitude() {
+      //Grabs the user location using their web browser
+      function getUserLocation() {
         return new Promise((resolve, reject) => {
           if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
               position => {
                 const latitude = position.coords.latitude;
-                resolve(latitude);
+                const longitude = position.coords.longitude;
+                resolve({ latitude, longitude });
               },
               error => {
                 reject(error);
@@ -24,25 +23,57 @@ class Scene1 extends Phaser.Scene {
           }
         });
       }
+      //Grabs the temperature using an API key, based on the location provided by getUserLocation()
+      function fetchTemperature(latitude, longitude) {
+        return new Promise((resolve, reject) => {
+          const apiKey = "5c7f96f9f8484cb4ba1203818242404";
+          const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}`;
+      
+          fetch(url)
+            .then(response => {
+              if (!response.ok) {
+                throw new Error("Failed to fetch weather data.");
+              }
+              return response.json();
+            })
+            .then(data => {
+              const temperature = data.current.temp_f;
+              resolve(temperature);
+            })
+            .catch(error => {
+              reject(error);
+            });
+        });
+      }
 
-      getUserLatitude().then(latitude => {
-        if(latitude >= 30){
+      //runs getUserLocation and fetchTemperature to get the temperature
+      getUserLocation().then(location => {
+        return fetchTemperature(location.latitude, location.longitude);
+      })
+
+      //Determines what version of the game will be played based on the temperature. If its above 70F then the desert
+      //version is run, below 70F runs the ocean variation.
+      .then(temperature => {
+        if(temperature >= 70){
           this.load.image("background", "assets/images/Desert.png");
+          this.load.image("monster", "assets/images/crow.png");
+          this.load.image("player", "assets/images/drone.png");
+          this.load.audio("music", "assets/sounds/desertmusic.mp3");
         }else{
           this.load.image("background", "assets/images/underwater.jpg");
+          this.load.image("monster", "assets/images/monster.png");
+          this.load.image("player", "assets/images/player.webp");
+          this.load.audio("music", "assets/sounds/oceanmusic.mp3");
         }
       });
 
     //asset preload
     this.load.image("coin", "assets/images/coin.png");
-    this.load.image("monster", "assets/images/monster.png");
-    this.load.image("player", "assets/images/player.webp");
-
+    
     //Sound/bitmap preload
     this.load.bitmapFont("pixelFont", "assets/font/font.png", "assets/font/font.xml");
     this.load.audio("audio_damage", "assets/sounds/damage.mp3");
-    this.load.audio("audio_pickup", ["assets/sounds/pickup.ogg", "assets/sounds/pickup.mp3"]);
-    this.load.audio("music", "assets/sounds/oceanmusic.mp3");
+    this.load.audio("audio_pickup", ["assets/sounds/pickup.ogg", "assets/sounds/pickup.mp3"]); 
   }
 
   create() {
