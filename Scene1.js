c56d77d687ba2031e986e7531f851a2df12b149b
bohmanwl <112824679+bohmanwl@users.noreply.github.com>
2024-04-24 00:51:44 -0400

    Removed old unused assets and replaced music to be more "ocean level like"

--- Scene1.js
+++ Scene1.js
@@ -6,15 +6,15 @@ class Scene1 extends Phaser.Scene {
     preload(){
     //asset preload
     this.load.image("background", "assets/images/background.png");
-    this.load.image("coin", "assets/images/coin2.png");
-    this.load.image("power-up", "assets/images/monster2.png");
+    this.load.image("coin", "assets/images/coin.png");
+    this.load.image("power-up", "assets/images/monster.png");
     this.load.image("player", "assets/images/player.webp");
-    
+
     //Sound/bitmap preload
     this.load.bitmapFont("pixelFont", "assets/font/font.png", "assets/font/font.xml");
     this.load.audio("audio_damage", "assets/sounds/damage.mp3");
     this.load.audio("audio_pickup", ["assets/sounds/pickup.ogg", "assets/sounds/pickup.mp3"]);
-    this.load.audio("music", ["assets/sounds/sci-fi_platformer12.ogg", "assets/sounds/sci-fi_platformer12.mp3"]);
+    this.load.audio("music", "assets/sounds/oceanmusic.mp3");
   }
 
   create() {
