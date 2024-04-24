a5a74f50732e2fb6446f32b9a7649bb595ef001b
Sumi <allen4c5@mail.uc.edu>
2024-04-23 20:47:54 -0400

    background implementation updates

--- Scene1.js
+++ Scene1.js
@@ -46,5 +46,45 @@ class Scene1 extends Phaser.Scene {
   create() {
     this.add.text(20, 20, "Loading game...");
     this.scene.start("playGame");
+    function geoFindMe() {
+      const status = document.querySelector("#status");
+      const mapLink = document.querySelector("#map-link");
+
+      mapLink.href = "";
+      mapLink.textContent = "";
+      let imageKey = "";
+
+      function success(position) {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+
+          status.textContent = "";
+          if (latitude > 30) {
+              temperatureText = "hot";
+              imageKey = "hot";
+          } else if (latitude >= 10 && latitude <= 30) {
+              temperatureText = "warm";
+               imageKey = "warm";
+          } else {
+              temperatureText = "cold";
+              imageKey = "cold";
+          }
+          mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+          mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} ° , ${temperatureText}`;
+      }
+
+      function error() {
+          status.textContent = "Unable to retrieve your location";
+      }
+
+      if (!navigator.geolocation) {
+          status.textContent = "Geolocation is not supported by your browser";
+      } else {
+          status.textContent = "Locating…";
+          navigator.geolocation.getCurrentPosition(success, error);
+      }
+  }
+
+  document.querySelector("#find-me").addEventListener("click", geoFindMe);
   }
 }
